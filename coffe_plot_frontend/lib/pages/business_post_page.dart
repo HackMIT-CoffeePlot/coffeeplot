f832469800d6a488328e3d77356391fdf49285d9
Deepak Govindarajan <krishnadeepakcs123@gmail.com>
2023-09-16 21:30:37 -0400

    feat: setup the tab pages

---
+++ coffe_plot_frontend/lib/pages/business_post_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BusinessPostPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Business Post Page'),
+      ),
+      body: Center(
+        child: Text('Business Post Page'),
+      ),
+    );
+  }
+}
---
+++ coffe_plot_frontend/lib/pages/stats_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stats Page'),
+      ),
+      body: Center(
+        child: Text('Stats Page'),
+      ),
+    );
+  }
+}
