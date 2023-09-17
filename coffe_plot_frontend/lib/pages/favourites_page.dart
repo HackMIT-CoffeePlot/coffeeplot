a791c41791f761f4c9c4eb6cae43b6095fa47764
ethanchan11 <eethan.chan11@gmail.com>
2023-09-17 07:06:49 -0400

    blah blah blah

--- coffe_plot_frontend/lib/pages/favourites_page.dart
+++ coffe_plot_frontend/lib/pages/favourites_page.dart
@@ -4,11 +4,18 @@ import './searchbar.dart';
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xFFFFFEFA),
       body: ListView(children: <Widget>[
-        //your content goes
-        Center(child: BarSearch())
+        //your content goes here
+        Center(
+          child: Container(
+            width: deviceSize.width * 0.9,  // adjust size using 0.x where x is a decimal number between 0 and 1
+            // height: deviceSize.height * 0.x, (Uncomment and adjust if you also want to adjust the height)
+            child: BarSearch(),
+          ),
+        )
       ]),
     );
   }
--- coffe_plot_frontend/lib/pages/listings_page.dart
+++ coffe_plot_frontend/lib/pages/listings_page.dart
@@ -4,16 +4,25 @@ import './searchbar.dart';
 class ListingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xFFFFFEFA),
       body: ListView(children: <Widget>[
-        //your content goes
-        Center(child: BarSearch()),
+        //your content goes here
+        Center(
+          child: Container(
+            width: deviceSize.width * 0.9, // adjust size using 0.x where x is a decimal number between 0 and 1
+            // height: deviceSize.height * 0.x, (Uncomment and adjust if you also want to adjust the height)
+            child: BarSearch(),
+          ),
+        ),
         Align(
-          alignment: Alignment.centerRight, // Align the text to the right
+          alignment: Alignment.centerLeft, // Align the text to the right
           child: Text(
             'Near Me',
             style: TextStyle(
+              color: Colors.green,
+              fontSize: 40,
               fontFamily: 'GraphikLCG-Semibold', // Use the font family name
             ),
           ), // Add this line
