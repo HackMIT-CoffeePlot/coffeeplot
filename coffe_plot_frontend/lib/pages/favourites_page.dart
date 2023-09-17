744423a8ffd4d3389a9f12b794245deb57e3ae1c
ethanchan11 <eethan.chan11@gmail.com>
2023-09-17 03:54:23 -0400

    background colour

--- coffe_plot_frontend/lib/pages/favourites_page.dart
+++ coffe_plot_frontend/lib/pages/favourites_page.dart
@@ -4,6 +4,7 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFFFEFA),
       body: ListView(children: <Widget>[
         //your content goes
         Center(child: Text('Favorites Page'))
